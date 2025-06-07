ff1715e9aaf86463e706ea34e7cf34c2210d3ad8
aditya shourya <adi@adityas-MacBook-Air.local>
2025-06-08 01:41:25 +0200

    Added sparse animation and fixed prev axes

--- scratchpad/animations/quant_animation.py
+++ scratchpad/animations/quant_animation.py
@@ -1,4 +1,5 @@
 import os
+from turtle import left
 from typing_extensions import runtime
 from manim import *
 # config.background_color = WHITE
@@ -11,17 +12,19 @@ def qbits(x, b=6.0, e=-8.0):
     x_clipped = np.clip(x_scaled, -1 * np.exp2(b - 1), np.exp2(b - 1) - 1)
     x_round = np.round(x_clipped)
     result = np.exp2(e) * x_round
-    return (result, x_scaled, x_clipped, x_round),(-1*np.exp2(b-1), np.exp2(b-1))
+    return (result, x_scaled, x_clipped, x_round),(-1*np.exp2(b-1), np.exp2(b-1)-1)
 
 class QuantizationAnimation(MovingCameraScene):
+    
+
     def construct(self):
         # dont change this... [took a while to find a good value for animation]
         e_bit = -3.5
         b_bit = 3.5
         x_input = np.linspace(-2,2,100)
         y_init = x_input
-        (y_resut,y1,y2,y3),(cl_l,cl_r) = qbits(x_input,b_bit,e_bit)
-        print(cl_l,cl_r)
+        (y_result,y1,y2,y3),(cl_l,cl_r) = qbits(x_input,b_bit,e_bit)
+        print("???",cl_l,cl_r)
 
         ax1 = Axes(
             x_range=[x_input[0], x_input[-1], 5],
@@ -33,12 +36,36 @@ class QuantizationAnimation(MovingCameraScene):
             tips=True,
             )
         ax1_copy =ax1.copy()
+
+        ax_labels = ax1.get_axis_labels(x_label="x", y_label="q(x)")
+
+        # code
+        code = """
+            def qbits(x, b, e):
+                b = torch.relu(b)
+                x_scaled = x / torch.exp2(e)
+                x_clipped = torch.clip(x_scaled, -1 * torch.exp2(b - 1), torch.exp2(b - 1) - 1)
+                x_round = np.round(x_clipped)
+                result = np.exp2(e) * x_round
+                return result
+        """
+
+        rendered_code = Code(
+        code_string=code,
+        language="python",
+        ).scale(0.3)
+        rendered_code.to_corner(DR)
+
         
 
         # first step
-        plane1=NumberPlane(x_range=(x_input[0],x_input[-1],1),
-                           y_range=(y_init[0],y_init[-1],1),
-                           background_line_style={"stroke_width": 1, "stroke_color": GRAY_BROWN},)
+        # plane1=NumberPlane(x_range=(-10,10,1),
+        #                    y_range=(-50,50,1),
+        #                    background_line_style={"stroke_width": 1, "stroke_color": GRAY},)
+
+        plane1=NumberPlane(x_range=(x_input[0],x_input[-1]),
+                           y_range=(y_init[0],y_init[-1]),
+                           background_line_style={"stroke_width": 1, "stroke_color": GRAY},)
         graph1 = ax1.plot_line_graph(x_input, y_init, line_color=ORANGE,vertex_dot_radius=0)
 
         # scale it down
@@ -55,7 +82,7 @@ class QuantizationAnimation(MovingCameraScene):
 
         #clip it 
         ax3 = Axes(
-            x_range=[cl_l,cl_r],
+            x_range=[x_input[0],x_input[-1]],
             y_range=[y1[0], y1[-1]],
             x_length=5,
             y_length=7,
@@ -67,7 +94,7 @@ class QuantizationAnimation(MovingCameraScene):
 
         #quantize
         ax4 = Axes(
-            x_range=[cl_l,cl_r],
+            x_range=[x_input[0],x_input[-1]],
             y_range=[y1[0], y1[-1]],
             x_length=5,
             y_length=7,
@@ -79,28 +106,42 @@ class QuantizationAnimation(MovingCameraScene):
 
         #rescale it back
         ax5 = Axes(
-            x_range=[cl_l,cl_r],
-            y_range=[y_resut[0], y_resut[-1]],
+            x_range=[x_input[0],x_input[-1]],
+            y_range=[y_result[0], y_result[-1]],
             x_length=5,
             y_length=5,
             x_axis_config={"numbers_to_include": x_input[::len(x_input//10)]},
             y_axis_config={"numbers_to_include": y1[::len(y1//10)]},
             tips=True,
         )
-        graph5 = ax5.plot_line_graph(x_input,y_resut,line_color=PURPLE,vertex_dot_radius=0)
+        graph5 = ax5.plot_line_graph(x_input,y_result,line_color=PURPLE,vertex_dot_radius=0)
+
+
 
         # linspaced inputs
-        title = Text(f"Quantization Fnction\n{b_bit=}{e_bit=}",font_size=14)
+        title = Text(f"Quantization Fnction\n{b_bit=}{e_bit=}",font_size=14,font="Simple Nerd Font")
         title.to_corner(UR)
 
+        author = Text("-Adi",font_size=5,font="Simple Nerd Font")
+        author.to_corner(DL)
 
-        graph1_title =Text("Linspaced Input (-3,3)",font_size=20,font="Simple Nerd Font",color=ORANGE).to_corner(UL)
-        graph2_title =Text("Scale UP \n *1/torch.exp2(e) ",font_size=20,font="Simple Nerd Font",color=RED).to_corner(UL)
-        graph3_title =Text("CLIP \n (-1*torch.exp2(b-1), torch.exp2(b-1)-1)",font="Simple Nerd Font",font_size=20,color=YELLOW).to_corner(UL)
-        graph4_title =Text("Quantize torch.round (STE)",font_size=20,font="Simple Nerd Font",color=GREEN).to_corner(UL)
-        graph5_title =Text("Scale Down * torch.exp2(e)",font_size=20,font="Simple Nerd Font",color=PURPLE).to_corner(UL)
 
-        self.add(plane1,ax1,title)
+        # graph1_title =Text("Linspaced Input (-3,3)",font_size=20,font="Simple Nerd Font",color=ORANGE).to_corner(UL)
+        # graph2_title =Text("Scale UP \n *1/torch.exp2(e) ",font_size=20,font="Simple Nerd Font",color=RED).to_corner(UL)
+        # graph3_title =Text("CLIP \n (-1*torch.exp2(b-1), torch.exp2(b-1)-1)",font="Simple Nerd Font",font_size=20,color=YELLOW).to_corner(UL)
+        # graph4_title =Text("Quantize torch.round (STE)",font_size=20,font="Simple Nerd Font",color=GREEN).to_corner(UL)
+        # graph5_title =Text("Scale Down * torch.exp2(e)",font_size=20,font="Simple Nerd Font",color=PURPLE).to_corner(UL)
+        
+        graph1_title = Text("Linspaced Input (-3,3)", font_size=20, font="Simple Nerd Font", color=ORANGE).next_to(graph1,LEFT)
+        graph2_title = MathTex(r"\text{Scale} \frac{1}{2^e}", font_size=20,  color=RED).next_to(graph2,LEFT)
+        graph3_title = MathTex(r"\text{clip}(-2^{b-1}, 2^{b-1} - 1)", font_size=20,  color=YELLOW).next_to(graph3,LEFT)
+        # Alternatively, if you want to keep the torch code look:
+        # graph3_title = Text("CLIP \n (-1*torch.exp2(b-1), torch.exp2(b-1)-1)", font="Simple Nerd Font", font_size=20, color=YELLOW).next_to(graph3)
+        graph4_title = Text("Quantize torch.round\n(STE)", font_size=20,font="Simple Nerd Font",  color=GREEN).next_to(graph4,LEFT)
+        graph5_title = MathTex(r"\text{Scale Down}  2^e", font_size=20,  color=PURPLE).next_to(graph5,LEFT)
+
+
+        self.add(plane1,ax1,ax_labels,title,author)
         self.wait(2)
         self.play(FadeIn(graph1),Write(graph1_title))
         self.wait(2)
@@ -108,7 +149,14 @@ class QuantizationAnimation(MovingCameraScene):
         self.play(Transform(ax1,ax2),ScaleInPlace(plane1,np.exp2(e_bit)),Transform(graph1,graph2),Transform(graph1_title,graph2_title))
         self.wait(2)
         # clipped
+
+
+        clip_line1 = ax2.plot(lambda x:cl_l,color=YELLOW)
+        clip_line2 = ax2.plot(lambda x:cl_r,color=YELLOW)
+        self.play(Create(clip_line1),Create(clip_line2))
+
         self.play(Transform(graph1,graph3),Transform(graph1_title,graph3_title))
+        self.play(FadeOut(clip_line1),FadeOut(clip_line2))
         self.wait(2)
         # quantize
         self.play(Transform(graph1_title,graph4_title))
---
+++ scratchpad/animations/quant_sparse.py
@@ -0,0 +1,167 @@
+
+import os
+from tkinter import RIGHT
+import manim
+import numpy as np
+
+def qbits(x, b: float = 6.0, e: float = -8.0):
+    b = b if b > 0 else 0
+    x_scaled = x / np.exp2(e)
+    x_clipped = np.clip(x_scaled, -1 * np.exp2(b - 1), np.exp2(b - 1) - 1)
+    x_round = np.round(x_clipped)
+    result = np.exp2(e) * x_round
+    return result
+
+class toSparse(manim.MovingCameraScene):
+    def construct(self):
+        ax = manim.Axes(
+            x_range=[-2, 2, 1],
+            y_range=[-2, 2, 1],
+            x_length=6,
+            y_length=6,
+            axis_config={"include_tip": False}
+        )
+
+        # Labels for axes
+        ax_labels = ax.get_axis_labels(x_label="x", y_label="q(x)")
+
+        plane=manim.NumberPlane(x_range=(-2,2),
+                           y_range=(-2,2),
+                           background_line_style={"stroke_width": 1, "stroke_color": manim.GRAY},)
+
+        # ValueTrackers for b and e
+        b_tracker = manim.ValueTracker(3.5)
+        e_tracker = manim.ValueTracker(-3.5)
+
+        # Initial plot
+        def get_quant_plot():
+            x_vals = np.linspace(-2, 2, 100)
+            y_vals = qbits(x_vals, b_tracker.get_value(), e_tracker.get_value())
+            return ax.plot_line_graph(x_vals, y_vals, line_color=manim.PURPLE,vertex_dot_radius=0)
+
+        quant_graph = get_quant_plot()
+
+        # b and e value displays
+        b_display = manim.DecimalNumber(b_tracker.get_value(), num_decimal_places=2).next_to(ax, manim.LEFT).shift(manim.UP * 0.5)
+        b_label = manim.Text("b =", font="Simple Nerd Font", font_size=24).next_to(b_display, manim.LEFT)
+
+        e_display = manim.DecimalNumber(e_tracker.get_value(), num_decimal_places=2).next_to(ax, manim.LEFT).shift(manim.DOWN * 0.5)
+        e_label = manim.Text("e =", font="Simple Nerd Font", font_size=24).next_to(e_display, manim.LEFT)
+
+        # titles
+        title1 = manim.MarkupText(
+        f"""
+        Bit Depth: <span fgcolor="{manim.YELLOW}">Normal</span>
+        Exp Depth: <span fgcolor="{manim.YELLOW}">Normal</span>
+        Improves:  <span fgcolor="{manim.YELLOW}">Normal</span>
+        TradeOff:  <span fgcolor="{manim.YELLOW}">Normal</span>
+        """, color=manim.WHITE,font_size=15,font="Simple Nerd Font",
+        ).next_to(ax,manim.RIGHT).shift(manim.RIGHT)
+
+        title2 = manim.MarkupText(
+        f"""
+        Bit Depth: <span fgcolor="{manim.RED}">High</span>
+        Exp Depth: <span fgcolor="{manim.YELLOW}">Normal</span>
+        Improves:  <span fgcolor="{manim.GREEN}">More Coverage</span>
+        TradeOff:  <span fgcolor="{manim.YELLOW}">Normal</span>
+        """, color=manim.WHITE,font_size=15,font="Simple Nerd Font",
+        ).next_to(ax,manim.RIGHT).shift(manim.RIGHT)
+
+        title3 = manim.MarkupText(
+        f"""
+        Bit Depth: <span fgcolor="{manim.RED}">HIGH</span>
+        Exp Depth: <span fgcolor="{manim.RED}">HIGH</span>
+        Improves:  <span fgcolor="{manim.GREEN}">Precision</span>
+        TradeOff:  <span fgcolor="{manim.RED}">Input Coverage</span>
+        """, color=manim.WHITE,font_size=15,font="Simple Nerd Font",
+        ).next_to(ax,manim.RIGHT).shift(manim.RIGHT)
+
+        title4 = manim.MarkupText(
+        f"""
+        Bit Depth: <span fgcolor="{manim.RED}">HIGH</span>
+        Exp Depth: <span fgcolor="{manim.GREEN}">LOW</span>
+        Improves:  <span fgcolor="{manim.GREEN}">Storage</span>
+        TradeOff:  <span fgcolor="{manim.RED}">Less Precise</span>
+        """, color=manim.WHITE,font_size=15,font="Simple Nerd Font",
+        ).next_to(ax,manim.RIGHT).shift(manim.RIGHT)
+
+        title5 = manim.MarkupText(
+        f"""
+        Bit Depth: <span fgcolor="{manim.GREEN}">LOW</span>
+        Exp Depth: <span fgcolor="{manim.GREEN}">LOW</span>
+        Improves:  <span fgcolor="{manim.GREEN}">Storage</span>
+        TradeOff:  <span fgcolor="{manim.RED}">Input Coverage</span>
+        """, color=manim.WHITE,font_size=15,font="Simple Nerd Font",
+        ).next_to(ax,manim.RIGHT).shift(manim.RIGHT)
+
+        title6 = manim.MarkupText(
+        f"""
+        Bit Depth: <span fgcolor="{manim.YELLOW}">0</span>
+        Exp Depth: <span fgcolor="{manim.YELLOW}">Any</span>
+        Quant:     <span fgcolor="{manim.YELLOW}">Sparse</span>
+        """, color=manim.WHITE,font_size=20,font="Simple Nerd Font",
+        ).next_to(ax,manim.RIGHT).shift(manim.RIGHT)
+
+
+        self.add(ax, ax_labels, quant_graph, b_display, b_label, e_display, e_label,plane,title1)
+
+        def update_graph(mobj):
+            mobj.become(get_quant_plot())
+
+        def update_b(mobj):
+            mobj.set_value(b_tracker.get_value())
+
+        def update_e(mobj):
+            mobj.set_value(e_tracker.get_value())
+
+        quant_graph.add_updater(update_graph)
+        b_display.add_updater(update_b)
+        e_display.add_updater(update_e)
+
+
+        # Animate: increase b (more precision)
+        self.play(manim.Indicate(b_display))
+        self.play(manim.Transform(title1,title2))
+        self.play(b_tracker.animate.set_value(6), run_time=3)
+        self.wait(0.5)
+
+        # Animate: improve e (more precision)
+        self.play(manim.Indicate(e_display))
+        self.play(manim.Transform(title1,title3))
+        self.play(e_tracker.animate.set_value(-8.0), run_time=3)
+
+        self.play(self.camera.frame.animate.scale(0.1).move_to(manim.ORIGIN), run_time=1)
+        self.wait(1.5)
+        self.play(self.camera.frame.animate.scale(10).move_to(manim.ORIGIN), run_time=1)
+
+        # Animate: decrease e (bad precision)
+        self.play(manim.Indicate(e_display))
+        self.play(manim.Transform(title1,title4))
+        self.play(e_tracker.animate.set_value(-1.0), run_time=3)
+        self.wait(0.5)
+
+        # Animate: decrease b (bad coverage)
+        self.play(manim.Indicate(b_display))
+        self.play(manim.Transform(title1,title5))
+        self.play(b_tracker.animate.set_value(1), run_time=3)
+        self.wait(0.5)
+
+        # Animate: set b to 0 (makes output sparse)
+        self.play(manim.Indicate(b_display))
+        self.play(manim.Transform(title1,title6))
+        self.play(b_tracker.animate.set_value(0), run_time=3)
+        self.wait(1)
+
+        # Animate: no place for mantissa
+        self.play(manim.Indicate(e_display))
+        self.play(e_tracker.animate.set_value(-8.0), run_time=3)
+        self.wait(0.5)
+
+        quant_graph.clear_updaters()
+        b_display.clear_updaters()
+        e_display.clear_updaters()
+
+        self.wait()
+
+if __name__ == "__main__":
+    os.system("manim -qh quant_sparse.py toSparse")
