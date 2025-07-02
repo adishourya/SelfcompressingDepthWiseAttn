3629286292607831c0a277e417ed2428b3f55359
adishourya <shourya.aditya10@gmail.com>
2025-07-02 18:32:41 +0200

    Pixel shuffling might discourage pruning

--- compression/qmodules/QConv.py
+++ compression/qmodules/QConv.py
@@ -58,7 +58,7 @@ class Qconv(torch.nn.Module):
         self.depth_bit = torch.nn.Parameter(self.depth_bit)
         ...
 
-    def size_layer_old(self):
+    def size_layer(self):
         """
         given by equation 4 : I*H*W * sum(b(i,l)
         Where O , I , H and W are the output, input, height, and
@@ -70,7 +70,7 @@ class Qconv(torch.nn.Module):
         size = torch.prod(prods) *  torch.sum(torch.relu(self.depth_bit))
         return size
 
-    def size_layer(self):
+    def _fakebits(self):
         """
         New size layer ... this used to be calculation for fake bits
         This is more agressive than old implementation
@@ -80,8 +80,6 @@ class Qconv(torch.nn.Module):
             ) 
 
 
-    def _fakebits(self):
-        return torch.sum(self.depth_bit)
 
     def _quantized_weight(self):
         b = torch.relu(self.depth_bit)
